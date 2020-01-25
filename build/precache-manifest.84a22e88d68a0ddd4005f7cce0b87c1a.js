b5b252439311bc8d7def3ace988d2a64318498b2
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-01-25 23:56:20 +0500

    Add files via upload

---
+++ build/precache-manifest.84a22e88d68a0ddd4005f7cce0b87c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6d4927c315217fe5df7ba669e48786",
+    "url": "/react-todo-list/build/index.html"
+  },
+  {
+    "revision": "738a71e6b9d7ac50070c",
+    "url": "/react-todo-list/build/static/css/main.984e82ef.chunk.css"
+  },
+  {
+    "revision": "35f968c12dbc91762e7a",
+    "url": "/react-todo-list/build/static/js/2.687fe696.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/react-todo-list/build/static/js/2.687fe696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "738a71e6b9d7ac50070c",
+    "url": "/react-todo-list/build/static/js/main.75571d0d.chunk.js"
+  },
+  {
+    "revision": "a4bb44b513c4eb401643",
+    "url": "/react-todo-list/build/static/js/runtime-main.444df7d3.js"
+  }
+]);
